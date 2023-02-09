6a1996a0b49966ee9c0124dbaa78ec5e10f54661
Dowon Kim <dwkim16@gmail.com>
2023-02-09 03:12:42 -0500

    initial react blog

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let post = 'Title.';
+  let [title, changeFn] = useState('Title 1');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>Write.</h4>
+      </div>
+      <div className="list">
+        <h4>{ title }</h4>
+        <p>Published on Feb 17</p>
+      </div>
     </div>
   );
 }
