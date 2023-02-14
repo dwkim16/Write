4d1a724fb2a8565dbc1fa7353c79941178e8d4c1
Dowon Kim <dwkim16@gmail.com>
2023-02-14 00:05:20 -0500

    created modal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
 function App() {
 
   let post = 'Title.';
-  let [title, changeFn] = useState('Title 1');
+  let [title, setTitle] = useState('Title 1');
+  let [like, setLike] = useState(0);
+  let [modal, setModal] = useState(false);
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>Write.</h4>
       </div>
+
       <div className="list">
-        <h4>{ title }</h4>
+        <h4 onClick={ () => {setModal(!modal)} }>{ title }<span onClick={ () => {setLike(like + 1)} }> 👍 </span>{ like }</h4>
         <p>Published on Feb 17</p>
       </div>
+
+      {
+        modal ? <Modal/> : null
+      }
+
+      
+
     </div>
   );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+        <h4>Title</h4>
+        <p>Date</p>
+        <p>Content</p>
+    </div>
+
+  )
 }
 
 export default App;
